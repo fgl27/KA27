exynos5420/deathly_adiutor_free

BCL: Add toggle to enable disable the workque

app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
@@ -720,6 +720,9 @@ public interface Constants {
 
     String[] LOGGER_ARRAY = {LOGGER_MODE, LOGGER_ENABLED, LOGGER_LOG_ENABLED};
 
+    // BCL
+    String BCL = "/sys/module/battery_current_limit/parameters/bcl_user_enable";
+
     // CRC
     String[] CRC_ARRAY = {
             "/sys/module/mmc_core/parameters/crc",
@@ -750,7 +753,7 @@ public interface Constants {
     String HOSTNAME_KEY = "net.hostname";
 
     String[][] MISC_ARRAY = {{VIB_LIGHT, VIB_ENABLE, SENSOR_IND_WAKELOCK, MSM_HSIC_HOST_WAKELOCK, WLAN_RX_WAKELOCK_DIVIDER,
-            MSM_HSIC_WAKELOCK_DIVIDER, LOGGER_ENABLED, DYNAMIC_FSYNC, GENTLE_FAIR_SLEEPERS, ARCH_POWER, 
+            MSM_HSIC_WAKELOCK_DIVIDER, LOGGER_ENABLED, DYNAMIC_FSYNC, GENTLE_FAIR_SLEEPERS, ARCH_POWER, BCL, 
 			POWER_SUSPEND_MODE, POWER_SUSPEND_STATE, TCP_AVAILABLE_CONGESTIONS, HOSTNAME_KEY},
             SMB135X_WAKELOCKS, WLAN_RX_WAKELOCKS, WLAN_CTRL_WAKELOCKS, WLAN_WAKELOCKS, VIBRATION_ARRAY, CRC_ARRAY, FSYNC_ARRAY};
 

fgl27/KA27

Conflicts:
	app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java

app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
@@ -740,6 +740,9 @@ public interface Constants {
 
     String[] LOGGER_ARRAY = {LOGGER_MODE, LOGGER_ENABLED, LOGGER_LOG_ENABLED};
 
+    // BCL
+    String BCL = "/sys/module/battery_current_limit/parameters/bcl_user_enable";
+
     // CRC
     String[] CRC_ARRAY = {
             "/sys/module/mmc_core/parameters/crc",
@@ -769,7 +772,7 @@ public interface Constants {
     Object[][] MISC_ARRAY = {
             VIBRATION_ARRAY,
             {VIB_LIGHT, VIB_ENABLE, SENSOR_IND_WAKELOCK, MSM_HSIC_HOST_WAKELOCK, WLAN_RX_WAKELOCK_DIVIDER,
-                    MSM_HSIC_WAKELOCK_DIVIDER, LOGGER_ENABLED, DYNAMIC_FSYNC, GENTLE_FAIR_SLEEPERS, POWER_SUSPEND_MODE,
+                    MSM_HSIC_WAKELOCK_DIVIDER, LOGGER_ENABLED, DYNAMIC_FSYNC, GENTLE_FAIR_SLEEPERS, BCL,  POWER_SUSPEND_MODE,
                     POWER_SUSPEND_STATE, TCP_AVAILABLE_CONGESTIONS, HOSTNAME_KEY},
             SMB135X_WAKELOCKS, WLAN_RX_WAKELOCKS, WLAN_CTRL_WAKELOCKS, WLAN_WAKELOCKS, CRC_ARRAY, FSYNC_ARRAY};
 