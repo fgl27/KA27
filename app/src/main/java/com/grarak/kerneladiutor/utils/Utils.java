fef15c105dd5e8cbe6c098def5996149c7cace92
bhb27 <fella.motog@gmail.com>
2016-06-30 19:46:48 -0300

    Utils: getSysfspath - Return an empty string https://github.com/yoinx/kernel_adiutor/commit/77a85b3f8c3281609809d2e7adc6ad8467f1e958

--- app/src/main/java/com/grarak/kerneladiutor/utils/Utils.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/Utils.java
@@ -537,12 +537,23 @@ public class Utils implements Constants {
         return false;
     }
 
+
     public static String getsysfspath(String[] paths) {
         for (int i = 0; i < paths.length; i++) {
             if (Utils.existFile(paths[i])) {
                 return paths[i];
             }
         }
-        return null;
+        return "";
+    }
+
+    //Helper function to get paths with integer format substitutions
+    public static String getsysfspath(String[] paths, int sub) {
+        for (int i = 0; i < paths.length; i++) {
+            if (Utils.existFile(String.format(paths[i], sub))){
+                return String.format(paths[i], sub);
+            }
+        }
+        return "";
     }
 }
