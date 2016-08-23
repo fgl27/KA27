864b0bd9d0e0f67605096669d3985c70432c013f
bhb27 <fglfgl27@gmail.com>
2016-08-23 17:24:19 -0300

    Settings: Add 0sec delay to apply on boot
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- app/src/main/java/com/grarak/kerneladiutor/fragments/other/SettingsFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/other/SettingsFragment.java
@@ -142,6 +142,7 @@ public class SettingsFragment extends RecyclerViewFragment {
         }
 
         final List<String> list = new ArrayList<>();
+        list.add(0 + getString(R.string.sec));
         for (int i = 5; i < 421; i *= 2)
             list.add(i + getString(R.string.sec));
 
